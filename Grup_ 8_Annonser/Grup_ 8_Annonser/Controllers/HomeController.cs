ea4002348aece088237d47aebd9df8ae2c7b178d
simonsedig <38769156+simonsedig@users.noreply.github.com>
2018-04-27 01:37:19 +0200

    git.add

--- Grup_ 8_Annonser/Grup_ 8_Annonser/Controllers/HomeController.cs
+++ Grup_ 8_Annonser/Grup_ 8_Annonser/Controllers/HomeController.cs
@@ -8,10 +8,66 @@ namespace Grup__8_Annonser.Controllers
 {
     public class HomeController : Controller
     {
+        //Genererar en klass från en entitet
+        LoginDBEntities LoginDB = new LoginDBEntities();
+        Grupp8_AnnonserEntities AnnonsDB = new Grupp8_AnnonserEntities();
+
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //Skapar inloggning som är kopplad till en databas
+        // taget från exempel .asp hv
+        // skapa inloggning via db
+        [HttpPost]
+        public ActionResult Login(Models.LoginModel thisLogin)
+        {
+            if (thisLogin.Username == null || thisLogin.Password == null)
+            {
+                ModelState.AddModelError("", "Du har inte fyllt in fälten");
+                return View();
+            }
+
+            bool validUser = false;
+
+            validUser = CheckUser(thisLogin.Username, thisLogin.Password);
+
+            if (validUser == true)
+            {
+                System.Web.Security.FormsAuthentication.RedirectFromLoginPage(thisLogin.Username, false);
+            }
+            ModelState.AddModelError("", "Fel inloggning");
+            return View();
+        }
+
+        private bool CheckUser(string username, string password)
+        {
+            //LINQ method
+            var anvandare = from rows in LoginDB.Users
+                            where rows.Username.ToUpper() == username.ToUpper()
+                            && rows.Password == password
+                            select rows;
+
+            // if inlog success, skicka sant. annars falsk
+            if (anvandare.Count() == 1)
+            {
+                var user = anvandare.FirstOrDefault();
+                Session["thisLogin"] = 1;
+                //Session[USER_ID] = user.Id;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
