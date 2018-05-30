3ec18356ef8d7e299f24ec648048f1da977d2db8
simonsedig <38769156+simonsedig@users.noreply.github.com>
2018-05-30 22:06:45 +0200

    git add

--- Grup_ 8_Annonser/Grup_ 8_Annonser/Controllers/HomeController.cs
+++ Grup_ 8_Annonser/Grup_ 8_Annonser/Controllers/HomeController.cs
@@ -68,56 +68,6 @@ namespace Grup__8_Annonser.Controllers
             return RedirectToAction("Read");
         }
 
-        //public ActionResult Login()
-        //{
-        //    return View();
-        //}
-
-        ////Skapar inloggning som är kopplad till en databas
-        //// taget från exempel .asp hv
-        //// skapa inloggning via db
-        //[HttpPost]
-        //public ActionResult Login(Models.LoginModel thisLogin)
-        //{
-        //    if (thisLogin.Username == null || thisLogin.Password == null)
-        //    {
-        //        ModelState.AddModelError("", "Du har inte fyllt in fälten");
-        //        return View();
-        //    }
-
-        //    bool validUser = false;
-
-        //    validUser = CheckUser(thisLogin.Username, thisLogin.Password);
-
-        //    if (validUser == true)
-        //    {
-        //        System.Web.Security.FormsAuthentication.RedirectFromLoginPage(thisLogin.Username, false);
-        //    }
-        //    ModelState.AddModelError("", "Fel inloggning");
-        //    return View();
-        //}
-
-        //private bool CheckUser(string username, string password)
-        //{
-        //    //LINQ method
-        //    var anvandare = from rows in LoginDB.Users
-        //                    where rows.Username.ToUpper() == username.ToUpper()
-        //                    && rows.Password == password
-        //                    select rows;
-
-        //    // if inlog success, skicka sant. annars falsk
-        //    if (anvandare.Count() == 1)
-        //    {
-        //        var user = anvandare.FirstOrDefault();
-        //        Session["thisLogin"] = 1;
-        //        return true;
-        //    }
-        //    else
-        //    {
-        //        return false;
-        //    }
-        //}
-
         ServiceReferenceLogin.LogginClient loggain = new ServiceReferenceLogin.LogginClient();
         public ActionResult Index()
         {
@@ -142,12 +92,5 @@ namespace Grup__8_Annonser.Controllers
             ModelState.AddModelError("", "Felaktig inloggning");
             return View();
         }
-        public ActionResult LogOut()
-        {
-            FormsAuthentication.SignOut();
-            Session.Abandon(); //rensar sessionen vid slutet av requesten
-            return RedirectToAction("Index", "Home");
-        }
-
     }
 }
