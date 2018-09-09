91e8598e18cdfcf6eb9eb6ca885f19f94692987b
simonsedig <38769156+simonsedig@users.noreply.github.com>
2018-09-09 13:36:08 +0200

    git add

--- HospitalDLL/HospitalDLL/Class1.cs
+++ HospitalDLL/HospitalDLL/Class1.cs
@@ -10,15 +10,16 @@ namespace HospitalDLL
     {
         public class Patient
         {
-            public string firstName;
-            public string lastName;
-            public int age;
-            public string[] conditions;
-            public string currentHospital;
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public int age { get; set; }
+            public string[] conditions { get; set; }
+            public string currentHospital { get; set; }
         }
 
         public class Business
         {
+            public Patient[] patients { get; set; }
             public string hospitalName = "Sacred Heart";
             public string adress = "12629 Riverside Drive";
             public string city = "North Hollywood";
